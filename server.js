0e914c5d79458c4fa6804610a399a373bfbd32da
código <joserolandovelascopena@gmail.com>
2025-11-25 19:27:15 -0600

    Actualizo mi app

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static('docs'));
 app.listen(port, () => console.log("Servidor corriendo en http://localhost:3000"));
 app.listen(port, () => console.log("|||||||||||||||||||||||||||||||||||||||||"));
 app.listen(port, () => console.log("App SmartTasks corrindo ヾ(⌐■_■)ノ♪ "));
