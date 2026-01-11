3735121508a437478e1c7f58977aa42c4de459c1
código <joserolandovelascopena@gmail.com>
2026-01-11 13:31:12 -0600

    Integro Theme

--- docs/assets/js/themeManager/theme.core.js
+++ docs/assets/js/themeManager/theme.core.js
@@ -25,9 +25,16 @@ export function applyTheme(theme) {
 
   const navMain = qs(".nav-main");
   const iconsNav = qsa(".funciones_smart i");
-  const navMovil = qs(".NavMovil ");
+  const navMovil = qs(".NavMovil");
 
   const perfil = qs(".Perfile");
+  const editarModalPerfil = qs(".ContentEditar ");
+  const contenidoEditarPerfil = qs(".foto_Perfil_header ");
+  const closeEditarPerfil = qs(".CerrarEditor_Foto i ");
+  const BottonSheetPerfil = qs(".ContentSheet");
+  const opcionesSheetPerfil = qs(".AccionesAvanzadasEditarFoto");
+  const textOpcionesShet = qsa(".opcionesEditarfoto p");
+
   const addTareasContenedor = qs(".subir_tarea");
   const contenidoAddTarea = qs(".contenido_main");
   const navAddTasks = qs(".Nav_addTasks");
@@ -36,7 +43,7 @@ export function applyTheme(theme) {
   const encabezadosSectionAddTasks = qsa(".Categoria h4");
   const sectionAddTkas2 = qs(".descrition_box");
   const inputAddTasks = safeEl("newTask");
-  const descripcionTasks = qs(".descrition_box textarea ");
+  const descripcionTasks = qs(".descrition_box textarea");
 
   const encabezadosNavMain = qsa(".funciones_smart p");
   const titleVisionGeneral = qs(".tasksProyecAll h2");
@@ -49,12 +56,15 @@ export function applyTheme(theme) {
   const cantidadTareasHome = qs(".candTasks");
 
   //Const colors
-  const white = "#fff";
-  const black = "#000";
-  const blackTenue = "#232627ff";
-  const gray = "#383838ff";
-  const blueTransparet = "#8cc7d41e";
-  const skyBlue = "rgb(0, 162, 255)";
+  const COLORS = {
+    white: "#fff",
+    whiteDark: "#aaaaaa",
+    black: "#000",
+    blackSoft: "#232627ff",
+    gray: "#383838ff",
+    blueTransparent: "#8cc7d41e",
+    skyBlue: "rgb(0, 162, 255)",
+  };
 
   if (theme === "light") {
     document.documentElement.setAttribute("data-theme", "light");
@@ -66,61 +76,97 @@ export function applyTheme(theme) {
     setActiveButton(darkBtn);
 
     if (navMain) {
-      navMain.style.background = black;
+      navMain.style.background = COLORS.black;
+      navMain.style.boxShadow = "0 0  3px rgb(48, 47, 47)";
     }
     if (navMovil) {
-      navMovil.style.background = black;
-      navMovil.style.boxShadow = "0 0  3px" + gray;
+      navMovil.style.background = COLORS.black;
+      navMovil.style.boxShadow = "0 0  3px rgb(27, 27, 27)";
     }
 
     if (titleNameApp) {
-      titleNameApp.style.color = white;
+      titleNameApp.style.color = COLORS.white;
     }
 
-    encabezadosNavMain.forEach((encabezados) => {
-      encabezados.style.color = white;
-    });
+    if (encabezadosNavMain.length) {
+      encabezadosNavMain.forEach((el) => {
+        el.style.color = COLORS.whiteDark;
+      });
+    }
 
     if (perfil) {
-      perfil.style.background = blackTenue;
-      perfil.style.borderLeft = "2px solid" + gray;
+      perfil.style.background = COLORS.blackSoft;
+      perfil.style.borderLeftColor = " rgb(58, 58, 58)";
     }
+
+    if (editarModalPerfil) {
+      editarModalPerfil.style.background = COLORS.blackSoft;
+      editarModalPerfil.style.color = COLORS.white;
+    }
+
+    if (contenidoEditarPerfil) {
+      contenidoEditarPerfil.style.background = COLORS.black;
+      BottonSheetPerfil.style.background = COLORS.blackSoft;
+      opcionesSheetPerfil.style.background = COLORS.blackSoft;
+      opcionesSheetPerfil.style.color = COLORS.white;
+    }
+
+    if (textOpcionesShet) {
+      textOpcionesShet.forEach((op) => {
+        op.style.color = COLORS.whiteDark;
+      });
+    }
+
+    if (closeEditarPerfil) {
+      closeEditarPerfil.style.color = COLORS.white;
+    }
+
     if (addTareasContenedor) {
-      addTareasContenedor.style.background = blackTenue;
-      contenidoAddTarea.style.background = blackTenue;
-      navAddTasks.style.color = white;
-      sectionAddTkas.style.background = black;
+      addTareasContenedor.style.background = COLORS.blackSoft;
+      contenidoAddTarea.style.background = COLORS.blackSoft;
+      navAddTasks.style.color = COLORS.white;
+      sectionAddTkas.style.background = COLORS.black;
 
-      sectionAddTkas1.style.background = black;
-      sectionAddTkas2.style.background = black;
-      descripcionTasks.style.background = blueTransparet;
-      descripcionTasks.style.color = white;
-      inputAddTasks.style.background = blueTransparet;
-      inputAddTasks.style.color = white;
+      sectionAddTkas1.style.background = COLORS.black;
+      sectionAddTkas2.style.background = COLORS.black;
     }
 
-    if (plusAdd) plusAdd.style.background = "rgba(0, 13, 255, 0.93)";
+    if (descripcionTasks) {
+      descripcionTasks.style.background = COLORS.blueTransparent;
+      descripcionTasks.style.color = COLORS.white;
+    }
+
+    if (inputAddTasks) {
+      inputAddTasks.style.background = COLORS.blueTransparent;
+      inputAddTasks.style.color = COLORS.white;
+    }
+
+    if (plusAdd) {
+      plusAdd.style.background = "rgba(0, 13, 255, 0.93)";
+    }
+
     encabezadosSectionAddTasks.forEach((encabezados) => {
-      encabezados.style.color = white;
+      encabezados.style.color = COLORS.white;
     });
+
     iconsNav.forEach((ico) => {
-      ico.style.color = "rgba(255, 255, 255, 0.93)";
+      ico.style.color = COLORS.whiteDark;
     });
 
     if (titleVisionGeneral) {
-      titleVisionGeneral.style.color = white;
+      titleVisionGeneral.style.color = COLORS.white;
     }
     if (fechaCantidadTasks) {
-      fechaCantidadTasks.style.background = blueTransparet;
+      fechaCantidadTasks.style.background = COLORS.blueTransparent;
     }
     if (dateNow || numberDateHome) {
-      dateNow.style.background = black;
-      numberDateHome.style.background = black;
-      numberDateHome.style.color = skyBlue;
+      dateNow.style.background = COLORS.black;
+      numberDateHome.style.background = COLORS.black;
+      numberDateHome.style.color = COLORS.skyBlue;
       numberDateHome.style.border = "2px solid rgb(0, 162, 255)";
     }
-    if (caches) {
-      cantidadTareasHome.style.background = black;
+    if (cantidadTareasHome) {
+      cantidadTareasHome.style.background = COLORS.black;
     }
   }
 
